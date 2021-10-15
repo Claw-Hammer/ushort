4be2b3c3b9d55eb8f8bf5264abe6cd60b0c773c6
Julio <jvca2005@gmail.com>
2021-10-15 16:56:34 -0400

    Adding the controller that will handle the redirection

---
+++ app/Http/Controllers/RedirectionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Url;
+
+class RedirectionController extends Controller
+{
+    /**
+     * Redirects from the shortened URL to the real URL
+     * and saves that visit into the Database
+     * @param string $url
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function show(string $url)
+    {
+        $link = Url::where('short_url', 'like', '%' . $url . '%')->first();
+        $link->number_of_visits += 1;
+        $link->save();
+
+        return redirect($link->real_url);
+    }
+}

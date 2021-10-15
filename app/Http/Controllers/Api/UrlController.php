c57044aed8354f39c1796a5114d9b9b4bf8635b7
Julio <jvca2005@gmail.com>
2021-10-15 13:09:10 -0400

    Creating the Api controller that will handle URLs

---
+++ app/Http/Controllers/Api/UrlController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Url;
+use Illuminate\Http\Request;
+
+class UrlController extends Controller
+{
+    //
+}

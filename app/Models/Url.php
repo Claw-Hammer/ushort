a00743f23ba0f97055b592d56e6189b524308954
Julio <jvca2005@gmail.com>
2021-10-15 11:48:07 -0400

    Creating the $fillable array. This allows a factory/seeder comb to mass storage of data in allowed fields

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Url extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'real_url',
+        'short_url',
+        'number_of_visits',
+        'nsfw'
+    ];
 }

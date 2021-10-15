d18eef2926d6899e368bb3469fe434f28257b7a2
Julio <jvca2005@gmail.com>
2021-10-15 19:02:51 -0400

    Adding the route for the showReal functionality

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ use \App\Http\Controllers\Api\UrlController;
 Route::group(['prefix' => 'v1/url'], function () {
     Route::get('/shortener', [UrlController::class, 'index'])->name('api.v1.url.shortener');
     Route::get('/top', [UrlController::class, 'showTop'])->name('api.v1.url.top');
+    Route::get('/real', [UrlController::class, 'showReal'])->name('api.v1.url.real');
 });

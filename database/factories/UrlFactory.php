42d7b939cdada54bbbda66c1ddc87217a3b51df8
ClawHammer <jvca2005@gmail.com>
2021-10-16 13:00:21 -0400

    Creating the Url factory to populate the database with data for testing

--- database/factories/UrlFactory.php
+++ database/factories/UrlFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Url;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class UrlFactory extends Factory
 {
@@ -22,7 +23,10 @@ class UrlFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'real_url' => $this->faker->url(),
+            'short_url' => env('APP_URL') . "/" . $this->faker->unique()->regexify('[a-zA-Z0-9]{3}'),
+            'number_of_visits' => $this->faker->numberBetween(0, 500),
+            'nsfw' => $this->faker->numberBetween(0, 1),
         ];
     }
 }

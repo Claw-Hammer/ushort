5a195d76df63aec30acc3a96bf4039c3b7cb61cb
ClawHammer <jvca2005@gmail.com>
2022-07-02 20:55:54 -0400

    Changing the method to a more secure 'POST'

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\UrlController;
 
 Route::group(['prefix' => 'v1/url'], function () {
-    Route::get('/shortener', [UrlController::class, 'index'])->name('api.v1.url.shortener');
+    Route::post('/shortener', [UrlController::class, 'index'])->name('api.v1.url.shortener');
     Route::get('/top', [UrlController::class, 'showTop'])->name('api.v1.url.top');
     Route::get('/real', [UrlController::class, 'showReal'])->name('api.v1.url.real');
 });
